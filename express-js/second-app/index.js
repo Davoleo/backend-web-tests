cd7725563c5e99f5f773d8c3726ee1e334c45e81
Davoleo <davoleo2001@gmail.com>
2020-03-24 21:57:29 +0100

    ejs - loops and conditionals

--- express-js/second-app/index.js
+++ express-js/second-app/index.js
@@ -15,6 +15,16 @@ app.get("/looking_at/:thing", function (req, res) {
     res.render("look.ejs", {thing: thing});
 });
 
+app.get("/posts", function (req, res) {
+    let posts = [
+        {title: "Post1", author: "Davoleo"},
+        {title: "Post2", author: "uwuBoi"},
+        {title: "Post3", author: "Matpac"}
+    ];
+
+    res.render("posts.ejs", {posts: posts})
+});
+
 app.listen(3333, function () {
     console.log("App initialized on port 3333")
 });
