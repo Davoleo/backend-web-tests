1d4e48c26aedde88bec6a4f0c1d06dcfdc56eb14
Davoleo <davoleo2001@gmail.com>
2020-05-31 16:53:17 +0200

    package.json & some syntax updates

--- express-js/apis/test.js
+++ express-js/apis/test.js
@@ -4,11 +4,12 @@
 
 const request = require("request");
 
-request('http://www.googledakjsdksj.com', function (error, response, body) {
+request('http://jsonplaceholder.typicode.com/users/1', (error, response, body) => {
     if (error) {
-        console.error("Something is wrong :/")
-        console.error(error);
+        console.error("Something is wrong :/", error)
     } else if (response.statusCode === 200) {
-        console.log(body);
+        const parsedBody = JSON.parse(body);
+        //Template literals with a string (ES6)
+        console.log(`${parsedBody.name} lives in ${parsedBody.address.city}`);
     }
 })
