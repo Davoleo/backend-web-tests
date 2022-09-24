fca4d2f41a78633e9f8a5a0bcafdf1e65a259117
Davoleo <dav@davoleo.net>
2022-09-24 14:45:12 +0200

    some ODM features - methods - statics - virtual - middleware

--- express-js/anilist/models/anime.js
+++ express-js/anilist/models/anime.js
@@ -17,6 +17,34 @@ const aniSchema = new mongoose.Schema({
     ]
 });
 
+//Adding functionality methods to model instances
+aniSchema.methods.logName = function () {
+    //Logs the name of the anime
+    console.log(this.name);
+}
+
+//Example Static Model Functionality
+aniSchema.statics.logAll = function () {
+    //Log all anime objects
+    return console.log(this.find());
+}
+
+//Virtuals: Properties that don't exist in the database, but that we can use in the model
+aniSchema.virtual('nameDesc').get(function () {
+    return `${this.name} - ${this.description}`
+});
+
+//Middleware: do custom things before after some events
+//To maintain the middleware chain we:
+//  Either we accept next as parameter and call it as a function at the end of our middleware,
+//  or we make sure to return a promise at the end of the middleware function
+aniSchema.pre('save', async function () {
+    console.log("about to save!!")
+});
+aniSchema.post('save', async function () {
+    console.log("just saved!!")
+});
+
 //Model
 const Anime = mongoose.model("Anime", aniSchema);
 
